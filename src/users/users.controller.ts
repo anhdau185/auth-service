d8e311926f0edd29e325e7fcd7a4907b4b742a24
Anh Dau <anh.dau@employmenthero.com>
2022-07-18 07:24:07 +0700

    chore: inject necessary dependencies

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
 
+import { UsersService } from './users.service';
+
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+}
