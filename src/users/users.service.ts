eea8df65276cb07e040d3a6ea26773606e2c019e
Anh Dau <redbeanboy98@gmail.com>
2022-07-19 10:53:35 +0700

    chore: rename user entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { UserEntity } from './user.entity';
+import { User } from './user.entity';
 import { UserDto } from './user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   createUser(user: UserDto) {
