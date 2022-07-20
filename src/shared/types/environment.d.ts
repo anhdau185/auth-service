0b4dfdeb602487019d42f277df8a4c572e6431c7
Anh Dau <anh.dau@employmenthero.com>
2022-07-20 07:26:15 +0700

    chore: correct env type declaration

--- src/shared/types/environment.d.ts
+++ src/shared/types/environment.d.ts
@@ -1,12 +1,13 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      HOST: string;
-      DATABASE_HOST: string;
-      DATABASE_PORT: string;
-      DATABASE_USERNAME: string;
-      DATABASE_PASSWORD: string;
-      DATABASE_NAME: string;
+      MODE?: 'dev' | 'prod' | 'test';
+      PORT?: string;
+      DATABASE_HOST?: string;
+      DATABASE_PORT?: string;
+      DATABASE_USERNAME?: string;
+      DATABASE_PASSWORD?: string;
+      DATABASE_NAME?: string;
     }
   }
 }
