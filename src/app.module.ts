df5b9ea00060e2b06720cf7c1fc80aeb0a3c4c87
Anh Dau <anh.dau@employmenthero.com>
2022-07-20 22:17:35 +0700

    feat: generate auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { getOrmConfig } from './db/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(getOrmConfig()), UsersModule],
+  imports: [TypeOrmModule.forRoot(getOrmConfig()), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
