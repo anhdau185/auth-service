0b15eb840c024b527df273e1712ad5d278d43b2c
Anh Dau <redbeanboy98@gmail.com>
2022-07-21 13:50:58 +0700

    feat: implement passport-local strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 
-import { AuthService } from './auth.service';
-import { CredentialsDto } from './credentials.dto';
+import { concealCredentials } from '../users/users.utils';
+import { LocalAuthGuard } from './local.guard';
+import { AuthenticatedRequest } from './auth.types';
 
 @Controller('auth')
 export class AuthController {
-  private readonly AUTHENTICATION_SUCCESS = 'Authentication successful';
-  private readonly AUTHENTICATION_FAILURE =
-    'Authentication failed, please check your credentials';
-
-  constructor(private readonly authService: AuthService) {}
-
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Body() credentials: CredentialsDto) {
-    const result = await this.authService.validateUser(
-      credentials.username,
-      credentials.password,
-    );
-
-    return {
-      result,
-      message: result
-        ? this.AUTHENTICATION_SUCCESS
-        : this.AUTHENTICATION_FAILURE,
-    };
+  @HttpCode(200)
+  login(@Req() req: AuthenticatedRequest) {
+    return concealCredentials(req.user);
   }
 }
