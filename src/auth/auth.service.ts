cc482b381a87dd538c942f779b8d761774c4445a
Anh Dau <redbeanboy98@gmail.com>
2022-07-21 12:26:55 +0700

    feat: add logic for validating a user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+import comparePasswords from '../shared/utils/comparePasswords';
+import { UsersService } from '../users/users.service';
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, password: string): Promise<boolean> {
+    const user = await this.usersService.findOneUser({ username });
+    const userNotExisting = user == null;
+
+    if (userNotExisting) return false;
+
+    const isPasswordCorrect = await comparePasswords(password, user.password);
+    return isPasswordCorrect;
+  }
+}
