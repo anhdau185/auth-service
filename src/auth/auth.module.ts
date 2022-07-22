253eed0d164b5c83e329b1c194b2e52fac6bb187
Anh Dau <redbeanboy98@gmail.com>
2022-07-23 01:07:01 +0700

    feat: send access_token to client after successful login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
+import loadEnv from '../shared/utils/loadEnv';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 
+loadEnv();
+
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: 60 },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
