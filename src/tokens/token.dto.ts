0c1a51f0973380d412383e6758338459e649e1bc
Anh Dau <anh.dau@employmenthero.com>
2022-07-24 17:38:21 +0700

    feat: implement save refresh token to db flow

---
+++ src/tokens/token.dto.ts
@@ -0,0 +1,7 @@
+export class TokenDto {
+  public token: string;
+  public userId: number;
+  public createdAt?: number;
+  public validUntil?: number;
+  public lastUsedAt?: number;
+}
--- src/tokens/tokens.service.ts
+++ src/tokens/tokens.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
+
+import hashData from '../shared/utils/hashData';
+import { Token } from './token.entity';
+import { TokenDto } from './token.dto';
 
 @Injectable()
-export class TokensService {}
+export class TokensService {
+  constructor(
+    @InjectRepository(Token)
+    private readonly tokensRepository: Repository<Token>,
+  ) {}
+
+  async saveNewTokenForUser(data: TokenDto): Promise<Token> {
+    await this.deleteIfExists({ userId: data.userId });
+
+    const hashedToken = await hashData(data.token);
+    const processedData: TokenDto = {
+      ...data,
+      token: hashedToken,
+    };
+
+    return this.tokensRepository.save(processedData);
+  }
+
+  private async deleteIfExists(where: FindOptionsWhere<Token>): Promise<void> {
+    const existingRecord = await this.tokensRepository.findOneBy(where);
+    if (existingRecord == null) return;
+    await this.tokensRepository.delete(existingRecord.id);
+  }
+}
