1be9907de288fea243f88284abbee3ec79f04e1e
Anh Dau <redbeanboy98@gmail.com>
2022-07-31 13:17:14 +0700

    Update env var names

--- src/db/config.ts
+++ src/db/config.ts
@@ -5,11 +5,11 @@ export const getOrmConfig = (): PostgresConnectionOptions => {
   loadEnv();
   return {
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     synchronize: false,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/db/migrations/*.js'],
--- src/shared/types/env.d.ts
+++ src/shared/types/env.d.ts
@@ -2,12 +2,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       MODE?: 'dev' | 'prod' | 'test';
+      HOST?: string;
       PORT?: string;
-      DATABASE_HOST?: string;
-      DATABASE_PORT?: string;
-      DATABASE_USERNAME?: string;
-      DATABASE_PASSWORD?: string;
-      DATABASE_NAME?: string;
+      POSTGRES_HOST?: string;
+      POSTGRES_PORT?: string;
+      POSTGRES_USER?: string;
+      POSTGRES_PASSWORD?: string;
+      POSTGRES_DB?: string;
       JWT_SECRET_KEY?: string;
       JWT_EXPIRATION_TIME?: string;
       JWT_REFRESH_SECRET_KEY?: string;
