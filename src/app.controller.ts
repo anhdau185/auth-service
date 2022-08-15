127f1a9f0c79838b3ce520f6560d07f82ca3d22c
Anh Dau <redbeanboy98@gmail.com>
2022-08-15 12:16:58 +0700

    feat(app): change hello text

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  hello(): string {
     return this.appService.getHello();
   }
 }
