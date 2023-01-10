1ccdba3da2f3f647e4a63399707d509baa3feb17
Anh Dau <redbeanboy98@gmail.com>
2023-01-10 22:48:48 +0700

    update(docker-compose): persist pgadmin data using a volume

--- src/shared/types/env.d.ts
+++ src/shared/types/env.d.ts
@@ -9,6 +9,10 @@ declare global {
       POSTGRES_USER?: string;
       POSTGRES_PASSWORD?: string;
       POSTGRES_DB?: string;
+      PGADMIN_DEFAULT_EMAIL?: string;
+      PGADMIN_DEFAULT_PASSWORD?: string;
+      PGADMIN_PORT?: string;
+      PGADMIN_LISTEN_PORT?: string;
       JWT_SECRET_KEY?: string;
       JWT_EXPIRATION_TIME?: string;
       JWT_REFRESH_SECRET_KEY?: string;
